5b3ba8cf3ff7dbebb0b33d862e285a74d96c890e
hsohi2 <harsimarsohi1@gmail.com>
2022-09-22 16:52:30 -0500

    Added button

--- app/src/main/java/com/example/cooperativedemo1/MainActivity.java
+++ app/src/main/java/com/example/cooperativedemo1/MainActivity.java
@@ -6,9 +6,18 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button = (Button)findViewById(R.id.btnButton);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_LONG);
+                toast.show();
+            }
+
     }
 }
